7ca3d5b69c96547fbdbf0c4f7e6fbfe5e0b4f03a
Nadja Brandys <nadiusza@gmail.com>
2020-03-29 20:35:48 +0200

    Add OrderForm, OrderSummary and containers

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import OrderSummary from '../../features/OrderSummary/OrderSummary';
+import {Row, Col} from 'react-flexbox-grid';
+import PropTypes from 'prop-types';
 
-const OrderForm =()=> (
+const OrderForm = props => (
 
 <Grid>
   <Row>
     <Col xs={12}>
       <PageTitle text='Trip options' />
-      <OrderForm tripCost={cost} />
+      <OrderSummary cost ={props.tripCost} options={props.options}/>
     </Col>
   </Row>
 </Grid>
 
 );
+OrderForm.propTypes = {
+  tripCost: PropTypes.string,
+  options: PropTypes.object,
+};
 
 export default OrderForm;
