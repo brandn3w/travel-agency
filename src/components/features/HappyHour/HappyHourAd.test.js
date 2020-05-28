bd8fa0bb3c1e567e62fbefe11043f8bf86e6d585
Nadja Brandys <nadiusza@gmail.com>
2020-05-29 00:11:40 +0200

    Add test checking of the component renders title and div from props

--- src/components/features/HappyHour/HappyHourAd.test.js
+++ src/components/features/HappyHour/HappyHourAd.test.js
@@ -2,16 +2,32 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import HappyHourAd from './HappyHourAd';
 
+const select = {
+    title: '.title',
+    promoDescription: '.promoDescription',
+  };
 
-
+  const mockProps ={
+    title: 'lorem ipsum',
+    promoDescription:'sit amet'
+  }
+  
 describe('Component HappyHourAd', () => {
     it('should render without crashing', () => {
         const component = shallow(<HappyHourAd />);
         expect(component).toBeTruthy();
     });
-});
 
-describe('Should render header and div correctly', () => {
+
+it('should render heading and description', () => {
+    const component = shallow(<HappyHourAd />);
     expect(component.exists(select.title)).toEqual(true);
-    expect(component.exists(select.countdown)).toEqual(true);
+    expect(component.exists(select.promoDescription)).toEqual(true);
+  });
+
+  it('should render title and div from props', ()=>{
+    const component=shallow(<HappyHourAd {...mockProps}/>);
+    expect(component.find(select.title).text()).toEqual(mockProps.title);
+expect(component.find(select.promoDescription).text()).toEqual(mockProps.promoDescription);
+  });  
 });
