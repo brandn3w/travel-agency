0bed51048c2cff5da9012bb5e247161baf7b8dca
Nadja Brandys <nadiusza@gmail.com>
2020-06-04 23:44:36 +0200

    import DaysToSummer to Hero

--- src/components/layout/Hero/Hero.js
+++ src/components/layout/Hero/Hero.js
@@ -2,9 +2,13 @@ import React from 'react';
 import styles from './Hero.scss';
 import PropTypes from 'prop-types';
 import HappyHourAd from '../../features/HappyHour/HappyHourAd';
+import DaysToSummer from '../../../utils/daysToSummer'
 
 const Hero = ({variant = '', titleText, imageSrc, ...otherProps}) => (
   <div {...otherProps} className={styles.component + variant.split(' ').map(name => ' ' + (styles[name] || name)).join('')}>
+    <div className={'.daysToSummer'} >
+    <DaysToSummer description={to summer!} days={{ one: 'day', many: 'days' }}/>
+    </div>
     <h2 className={styles.title}>{titleText}</h2>
     <img className={styles.image}  src={imageSrc} />
     <div className={styles.happyHour}>
